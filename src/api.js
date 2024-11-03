93409e9226ece0c7191b30024edd44e9feb8bef6
Karontian <karontianpch@gmail.com>
2024-11-03 15:43:17 -0600

    building step b ste 4

--- src/api.js
+++ src/api.js
@@ -4,7 +4,11 @@ const app = express();
 const router = express.Router();
 const cors = require('cors')
 // const mongoose = require('mongoose')
+const axios = require('axios')
 
+const NewCompany = require('./models/company')
+const NewDriver = require('./models/driver')
+const NewUser = require('./models/users')
 
 
 
