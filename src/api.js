8258531bf4002854af6d5e8dde15ff420742bdf3
Karontian <karontianpch@gmail.com>
2024-11-03 15:37:19 -0600

    building step b ste 1

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ const express = require("express");
 const serverless = require("serverless-http");
 const app = express();
 const router = express.Router();
+const cors = require('cors')
 
 
 router.get("/", (req, res) => {
